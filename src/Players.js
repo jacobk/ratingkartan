70bd6aac4340f3b18a82723f60e6252461dc48aa
jacob <jacob.kristhammar@sinch.com>
2021-12-07 21:34:13 +0100

    Closes #1

--- src/Players.js
+++ src/Players.js
@@ -21,28 +21,48 @@ export default function Players() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [selectedPlayers, setSelectedPlayers] = useState(playerList);
   const selectedRegions = searchParams.getAll("region");
+  const selectedTerms = searchParams.getAll("terms");
   const selectedOptions = regions.filter((region) =>
     selectedRegions.includes(region.code)
   );
+  const value = [
+    ...selectedOptions,
+    ...selectedTerms.map((t) => ({ term: true, name: t })),
+  ];
+
+  console.log("selected terms", selectedTerms);
+  console.log("selected regions", selectedRegions);
+
+  const filterByRegion = (regions, players) =>
+    regions.length == 0
+      ? players
+      : players.filter((p) =>
+          selectedRegions.some((r) =>
+            [p.municipalityCode, p.countyCode].includes(r)
+          )
+        );
+
+  const filterByTerms = (terms, players) => {
+    return terms.length == 0
+      ? players
+      : players.filter((p) =>
+          terms.some((t) => p.name.toLowerCase().includes(t.toLowerCase()))
+        );
+  };
 
   useEffect(() => {
     console.log("do");
     setSelectedPlayers(
       _.orderBy(
-        selectedRegions.length == 0
-          ? playerList
-          : playerList.filter((p) =>
-              selectedRegions.some((r) =>
-                [p.municipalityCode, p.countyCode].includes(r)
-              )
-            ),
+        filterByTerms(
+          selectedTerms,
+          filterByRegion(selectedRegions, playerList)
+        ),
         "stats[0].rating",
         ["desc"]
       )
     );
   }, [searchParams]);
-
-  // return <div>Spelare: {searchParams.get("region")}</div>;
 
   function renderRow(props) {
     const { index, style } = props;
@@ -51,41 +71,8 @@ export default function Players() {
     return <PlayerRow player={player} index={index + 1} style={style} />;
   }
 
-  function renderRowOld(props) {
-    const { index, style } = props;
-    const player = selectedPlayers[index];
-
-    return (
-      <React.Fragment>
-        <ListItem style={style} key={index} component="div" disablePadding>
-          <Grid container spacing={1} height={38}>
-            <Grid item xs={2}>
-              <Box display="flex" justifyContent="center">
-                <Typography variant="h6" style={{ fontFamily: "monospace" }}>
-                  {index + 1}.
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={2}>
-              <Box display="flex" justifyContent="center">
-                <Typography variant="body">{player.rating}</Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={6}>
-              <Box style={{ paddingTop: 1 }}>{player.name}</Box>
-              <Box style={{ fontSize: "0.7rem", color: "#AAA" }}>
-                {player.municipality}, {player.county}
-              </Box>
-            </Grid>
-            <Grid item xs={2}>
-              <a href={`https://www.pdga.com/player/${player.pdgaNumber}`}>
-                #{player.pdgaNumber}
-              </a>
-            </Grid>
-          </Grid>
-        </ListItem>
-      </React.Fragment>
-    );
+  function matchRegion(term) {
+    return regions.find((r) => r.name.toLowerCase() === term.toLowerCase());
   }
 
   console.log("render");
@@ -98,15 +85,37 @@ export default function Players() {
             id="tags-outlined"
             options={regions}
             getOptionLabel={(option) => option.name}
-            value={selectedOptions}
+            value={value}
+            freeSolo
             onChange={(evt, newValue) => {
-              setSearchParams({ region: newValue.map((v) => v.code) });
+              const query = {
+                region: [],
+                terms: [],
+              };
+              newValue.forEach((v) => {
+                if (_.isObject(v)) {
+                  if (v.term) {
+                    query.terms.push(v.name);
+                  } else {
+                    query.region.push(v.code);
+                  }
+                } else {
+                  const regionMatch = matchRegion(v);
+                  if (regionMatch) {
+                    query.region.push(regionMatch.code);
+                  } else {
+                    query.terms.push(v);
+                  }
+                }
+              });
+
+              return setSearchParams(query);
             }}
             renderInput={(params) => (
               <TextField
                 {...params}
-                label="Län &amp; Kommuner"
-                placeholder="Kommun eller Län"
+                label="Län, Kommuner &amp; Spelare"
+                placeholder="Kommun, Län, Spelare"
                 variant="standard"
               />
             )}
