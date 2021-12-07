df44bf20819a96fb0addd738ab12c6d07388f765
jacobk <kristhammar@gmail.com>
2021-12-07 22:59:52 +0100

    Closes #2

--- src/Dump.js
+++ src/Dump.js
@@ -6,9 +6,9 @@ export default function Dump() {
   return (
     <Stack>
       <PlayerRow player={playerList[0]} index={8888} />
-      <PlayerRow player={playerList[13]} index={888} />
+      <PlayerRow player={playerList[13]} index={889} />
       <PlayerRow player={playerList[100]} index={88} />
-      <PlayerRow player={playerList[1]} index={8} />
+      <PlayerRow player={playerList[1]} index={9} />
     </Stack>
   );
 }
